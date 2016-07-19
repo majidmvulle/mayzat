79bddbbea6dff436f8fedac74bc17926a77d2723
Kamil Bartoszek <kamiox@gmail.com>
2016-07-19 11:32:16 +0200

    Added ouch.mp3, ouch.ogg to Preloaders.js

--- src/Preloader.js
+++ src/Preloader.js
@@ -22,7 +22,7 @@ Ball.Preloader.prototype = {
 		this.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
 
 		this.load.audio('audio-bounce', ['audio/bounce.ogg', 'audio/bounce.mp3', 'audio/bounce.m4a']);
-        this.load.audio('audio-ouch', ['audio/ouch.m4a']);
+        this.load.audio('audio-ouch', ['audio/ouch.m4a', 'audio/ouch.mp3', 'audio/ouch.ogg']);
 	},
 	create: function() {
 		this.game.state.start('MainMenu');
